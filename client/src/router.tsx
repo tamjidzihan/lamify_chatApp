0be34c327efe4af16f851de66e8b1875a36bf894
tamjidzihan <tamjidzihan@gmail.com>
2024-12-27 23:09:54 +0600

    home 2024-12-27

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import AuthPage from "./pages/AuthPage";
-import AuthWrapper from "./context/AuthWrapper";
+import AuthWrapper from "./utils/AuthWrapper";
 
 
 
